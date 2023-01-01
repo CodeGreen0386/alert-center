12e6d71f28c6b9837bf50b9a6218ab5540a4eca4
_CodeGreen <mapope8703@gmail.com>
2023-01-01 13:43:13 -0500

    Update GUI when opened, moved loop into functions

--- control.lua
+++ control.lua
@@ -52,21 +52,6 @@ end
 
 script.on_init(initial_setup)
 
-local function open_gui(event)
-    local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
-    local gui = player.gui.screen.alert_center
-    if not gui then gui = create_gui(player) end
-    gui.visible = true
-    gui.bring_to_front()
-    player.opened = gui
-end
-
-script.on_event("alert-center", open_gui)
-script.on_event(defines.events.on_lua_shortcut, function (event)
-    if event.prototype_name ~= "alert-center" then return end
-    open_gui(event)
-end)
-
 script.on_event(defines.events.on_player_created, function(event)
     setup_player(game.get_player(event.player_index) --[[@as LuaPlayer]])
 end)
@@ -97,82 +82,100 @@ local function alert_caption(count, time)
 end
 
 --- @param player LuaPlayer
---- @param name string
-local function update_alerts(player, name)
-    local alert_type = defines.alert_type[name]
-    local polled_alerts = player.get_alerts{surface = player.surface, type = alert_type}
-    if not next(polled_alerts) then return end
-    local new_alerts = polled_alerts[player.surface.index][alert_type]
-    local refs = global.players[player.index]
-    --- @type table<AlertID,SavedAlert>
-    local alerts = refs.alerts[name]
-    --- @type table<GroupID,Group>
-    local groups = refs.groups[name]
-    local game_tick = game.tick
-    for _, new_alert in pairs(new_alerts) do
-        local position = new_alert.position or new_alert.target.position
-        local id = position.x..","..position.y --- @type AlertID
-        if alerts[id] then goto continue end
-        local alert = {count = 0} --- @type SavedAlert
-        alerts[id] = alert
-        for group_id, group in pairs(groups) do
-            local dist = vec.mag(vec.sub(group.position, position))
-            if dist <= 32 then
-                group.count = group.count + 1
-                alert.group = group_id
-                group.alerts[id] = new_alert
-                group.position = vec.add(vec.div(vec.sub(position, group.position), vec.new(group.count)), group.position)
-                group.tick = game_tick
-                goto continue
+local function update_alerts(player)
+    for name in pairs(alert_info) do
+        local alert_type = defines.alert_type[name]
+        local polled_alerts = player.get_alerts{surface = player.surface, type = alert_type}
+        if not next(polled_alerts) then return end
+        local new_alerts = polled_alerts[player.surface.index][alert_type]
+        local refs = global.players[player.index]
+        --- @type table<AlertID,SavedAlert>
+        local alerts = refs.alerts[name]
+        --- @type table<GroupID,Group>
+        local groups = refs.groups[name]
+        local game_tick = game.tick
+        for _, new_alert in pairs(new_alerts) do
+            local position = new_alert.position or new_alert.target.position
+            local id = position.x..","..position.y --- @type AlertID
+            if alerts[id] then goto continue end
+            local alert = {count = 0} --- @type SavedAlert
+            alerts[id] = alert
+            for group_id, group in pairs(groups) do
+                local dist = vec.mag(vec.sub(group.position, position))
+                if dist <= 32 then
+                    group.count = group.count + 1
+                    alert.group = group_id
+                    group.alerts[id] = new_alert
+                    group.position = vec.add(vec.div(vec.sub(position, group.position), vec.new(group.count)), group.position)
+                    group.tick = game_tick
+                    goto continue
+                end
             end
+            local group = {count = 1, position = position, tick = game_tick, alerts = {new_alert}} --- @type Group
+            groups[id] = group
+            alert.group = id
+            ::continue::
         end
-        local group = {count = 1, position = position, tick = game_tick, alerts = {new_alert}} --- @type Group
-        groups[id] = group
-        alert.group = id
-        ::continue::
-    end
-    for alert_id, alert in pairs(alerts) do
-        alert.count = alert.count + 1
-        if alert.count >= 600 then
-            local group = groups[alert.group]
-            group.count = group.count - 1
-            alerts[alert_id] = nil
-            group.alerts[alert_id] = nil
-            if group.count <= 0 then
-                groups[alert.group] = nil
+        for alert_id, alert in pairs(alerts) do
+            alert.count = alert.count + 1
+            if alert.count >= 600 then
+                local group = groups[alert.group]
+                group.count = group.count - 1
+                alerts[alert_id] = nil
+                group.alerts[alert_id] = nil
+                if group.count <= 0 then
+                    groups[alert.group] = nil
+                end
             end
         end
     end
 end
 
 --- @param player LuaPlayer
---- @param alert_name string
-local function update_gui(player, alert_name)
+local function update_gui(player)
     local refs = global.players[player.index]
-    --- @type LuaGuiElement
-    local alert_flow = refs[alert_name]
-    --- @type table<GroupID,Group>
-    local groups = refs.groups[alert_name]
-    local game_tick = game.tick
+    for name in pairs(alert_info) do
+        --- @type LuaGuiElement
+        local alert_flow = refs[name]
+        --- @type table<GroupID,Group>
+        local groups = refs.groups[name]
+        local game_tick = game.tick
 
-    for id, group in pairs(groups) do
-        if not alert_flow[id] then
-            glib.add(alert_flow, {
-                args = {type = "button", name = id, index = 1, caption = alert_caption(group.count, 0), style = "list_box_item"},
-                style_mods = {horizontally_stretchable = true},
-                handlers = {[e.on_gui_click] = handlers.zoom_to_world}
-            })
-        else
-            alert_flow[id].caption = alert_caption(group.count, game_tick - group.tick)
+        for id, group in pairs(groups) do
+            if not alert_flow[id] then
+                glib.add(alert_flow, {
+                    args = {type = "button", name = id, index = 1, caption = alert_caption(group.count, 0), style = "list_box_item"},
+                    style_mods = {horizontally_stretchable = true},
+                    handlers = {[e.on_gui_click] = handlers.zoom_to_world}
+                })
+            else
+                alert_flow[id].caption = alert_caption(group.count, game_tick - group.tick)
+            end
         end
-    end
 
-    for _, group_id in pairs(alert_flow.children_names) do
-        if not groups[group_id] then
-            alert_flow[group_id].destroy()
+        for _, group_id in pairs(alert_flow.children_names) do
+            if not groups[group_id] then
+                alert_flow[group_id].destroy()
+            end
         end
     end
 end
+
+local function open_gui(event)
+    local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+    local gui = player.gui.screen.alert_center
+    if not gui then gui = create_gui(player) end
+    gui.visible = true
+    gui.bring_to_front()
+    player.opened = gui
+    update_gui(player)
+end
+
+script.on_event("alert-center", open_gui)
+script.on_event(defines.events.on_lua_shortcut, function (event)
+    if event.prototype_name ~= "alert-center" then return end
+    open_gui(event)
+end)
 
 --- @class AlertInfo
 --- @field icon SpritePath
@@ -187,13 +190,9 @@ local alert_info = {
 script.on_nth_tick(60, function(event)
     if event.tick == 0 then return end
     for _, player in pairs(game.connected_players) do
-        for alert in pairs(alert_info) do
-            update_alerts(player, alert)
-        end
+        update_alerts(player, alert)
         if not player.gui.screen.alert_center.visible then return end
-        for alert in pairs(alert_info) do
-            update_gui(player, alert)
-        end
+        update_gui(player, alert)
     end
 end)
 
